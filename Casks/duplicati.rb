ae5bd4313196ab378765dc500cdb7ac9b001bf20
Ted Carnahan <ted@tedcarnahan.com>
2018-12-05 17:11:37 -0600

    Clean up style

--- Casks/duplicati.rb
+++ Casks/duplicati.rb
@@ -2,6 +2,11 @@ cask "duplicati" do
   version "2.0.5.1,2020-01-18"
   sha256 "38509531cc9a007b98527af604f9a5faeb41b9221a79c7bd36e8846a32f1fda0"
 
+  url "https://updates.duplicati.com/beta/duplicati-#{version.before_comma}_beta_#{version.after_comma}.dmg"
+  appcast 'https://www.duplicati.com/download'
+  name 'Duplicati'
+  homepage 'https://www.duplicati.com/'
+
   # github.com/duplicati/duplicati/ was verified as official when first introduced to the cask
   url "https://github.com/duplicati/duplicati/releases/download/v#{version.before_comma}-#{version.before_comma}_beta_#{version.after_comma}/duplicati-#{version.before_comma}_beta_#{version.after_comma}.dmg"
   appcast "https://github.com/duplicati/duplicati/releases.atom"
